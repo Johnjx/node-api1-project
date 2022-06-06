0f4dfd593a08eed4283d95c3fc2c0c75fb76358f
John Ruddock <john.jxr@pm.me>
2022-06-06 15:58:56 -0400

    Server is up

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const Users = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    console.log('req received')
+    res.json({ message: 'your request has been received properly'})
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
