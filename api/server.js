4d5d7cb4983eba4a7cb384f41173de6053d128e7
John Ruddock <john.jxr@pm.me>
2022-06-06 16:16:41 -0400

    get users in place

--- api/server.js
+++ api/server.js
@@ -5,9 +5,20 @@ const Users = require('./users/model')
 const server = express();
 server.use(express.json())
 
+const URL = "/api/users"
+
 server.get('/', (req, res) => {
     console.log('req received')
     res.json({ message: 'your request has been received properly'})
+})
+
+server.get(URL, (req, res) => {
+    Users.find().then(result => {
+        res.json(result);
+    })
+    .catch(err => {
+        res.status(500).json({ message: "The users information could not be retrieved" })
+    })
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
